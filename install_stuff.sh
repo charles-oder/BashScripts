b33ede7e4239029f857d69a5edae199745b86e4f
Charles Oder Dev <charlesoder@Charless-MacBook-Pro.local>
2016-06-14 17:17:50 -0500

    Added more brew installs

--- install_stuff.sh
+++ install_stuff.sh
@@ -1,29 +1,20 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-read -n 1 -p 'Press enter to continue...'
-
 brew install Caskroom/cask/sublime-text
-
-read -n 1 -p 'Press enter to continue...'
 
 brew install git
 
-read -n 1 -p 'Press enter to continue...'
-
 brew install carthage
 
-read -n 1 -p 'Press enter to continue...'
-
-sudo gem install cocoapods
-
-read -n 1 -p 'Press enter to continue...'
+##sudo gem install cocoapods
 
 brew install cloc
 
-read -n 1 -p 'Press enter to continue...'
+##brew install swiftlint
 
-brew install swiftlint
+brew install Caskroom/cask/hipchat
 
-read -n 1 -p 'Press enter to continue...'
+brew install sqlitebrowser
+
 
 
