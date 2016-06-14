2c2484f822503bb479065c32d8c3adf7848227a8
Charles Oder Dev <charlesoder@Charless-MacBook-Pro.local>
2016-06-14 17:04:45 -0500

    Added install script for basic tools

---
+++ install_stuff.sh
@@ -0,0 +1,29 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+read -n 1 -p 'Press enter to continue...'
+
+brew install Caskroom/cask/sublime-text
+
+read -n 1 -p 'Press enter to continue...'
+
+brew install git
+
+read -n 1 -p 'Press enter to continue...'
+
+brew install carthage
+
+read -n 1 -p 'Press enter to continue...'
+
+sudo gem install cocoapods
+
+read -n 1 -p 'Press enter to continue...'
+
+brew install cloc
+
+read -n 1 -p 'Press enter to continue...'
+
+brew install swiftlint
+
+read -n 1 -p 'Press enter to continue...'
+
+
