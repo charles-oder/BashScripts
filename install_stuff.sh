55337fe6da5acaceab3a2b3588a3b13dcd283704
Charles Oder Dev <charlesoder@Charless-MacBook-Pro.local>
2016-06-14 17:44:31 -0500

    cleaned up install script

--- install_stuff.sh
+++ install_stuff.sh
@@ -1,3 +1,7 @@
+echo 'Install Xcode from the App Store'
+
+read -n 1 -p 'Press enter to continue...'
+
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew install Caskroom/cask/sublime-text
@@ -6,15 +10,13 @@ brew install git
 
 brew install carthage
 
-##sudo gem install cocoapods
-
 brew install cloc
 
-##brew install swiftlint
+brew install swiftlint
 
 brew install Caskroom/cask/hipchat
 
 brew install sqlitebrowser
 
-
+echo 'Install Cocoapods: "sudo gem install cocoapods"'
 
