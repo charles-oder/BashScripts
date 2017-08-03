60badaf55fd70a5677edba93ba3b513f9609c690
Charles Oder <charles.oder@dtn.com>
2017-08-03 14:36:02 -0500

    added email and username setup

--- setup_git.sh
+++ setup_git.sh
@@ -4,3 +4,9 @@ git config --global alias.ci commit
 git config --global alias.st status
 git config --global alias.unstage 'reset HEAD --'
 git config --global alias.last 'log -1 HEAD'
+
+read -p "enter git email: " EMAIL
+read -p "enter git username: " USERNAME
+
+git config --global user.email "$EMAIL"
+git config --global user.name "$USERNAME"
