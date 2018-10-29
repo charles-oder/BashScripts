1fa2cfd310ccff8f2d31151414bb56803505ed13
Charles Oder <charles.oder@dtn.com>
2018-10-29 10:10:17 -0500

    added commandline launch for android studio

--- .android-helper.sh
+++ .android-helper.sh
@@ -3,3 +3,12 @@ hungryJava() {
 	echo "export JAVA_TOOL_OPTIONS=\"-Xmx1024m -XX:MaxPermSize=512m -Xms512m\""
 	export JAVA_TOOL_OPTIONS="-Xmx1024m -XX:MaxPermSize=512m -Xms512m"
 }
+
+androidStudio() {
+	if [ -z "$1" ]; 
+		then DIR=$(pwd); 
+		else DIR=$1; 
+	fi
+	echo "Opening $DIR"
+	open -a /Applications/Android\ Studio.app/ $DIR
+}
