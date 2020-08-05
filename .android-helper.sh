b9c67bbf89f5ef8aa5ebe5620c5b8e909a5dd3f5
Charles <charles.oder@ehi.com>
2020-08-05 13:27:39 -0500

    added script for running adb test head unit for android auto

--- .android-helper.sh
+++ .android-helper.sh
@@ -12,3 +12,12 @@ androidStudio() {
 	echo "Opening $DIR"
 	open -a /Applications/Android\ Studio.app/ $DIR
 }
+
+runHeadUnit() {
+	OLD_DIR=$(pwd)
+	echo "Enable Developer Mode in the Android Auto Mobile App"
+	echo "Then start the head unit server from the Android Auto App settings"
+	ADB_LOCATION=$(which adb)
+	$ADB_LOCATION forward tcp:5277 tcp:5277
+	/Users/charlesoder/Library/Android/sdk/extras/google/auto/desktop-head-unit
+}
